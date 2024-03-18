60134f0fceeaadb17cf3f56cff822b9b3177d368
Gerrrrrria <1440054622@qq.com>
2024-03-18 15:13:19 +0800

    第二次实验报告

--- js-5/js/main.js
+++ js-5/js/main.js
@@ -1,35 +1,26 @@
 function f1(){
-    var score = prompt("请输入成绩：");
+    var month = Number(prompt("请输入月份："));
     var msg;
-    var re = Math.floor( score / 10 );
-    if(0 <= score && score <= 100){
-        switch(re){
-            case 0:
+        switch(month){
             case 1:
-            case 2:
             case 3:
-            case 4:
             case 5:
-                msg = "不及格";
-                break;    
-            case 6:
-                msg = "及格";
-                break;
             case 7:
-                msg = "一般";
-                break;
             case 8:
-                msg = "良好";
-                break;
-            case 9:
-                msg = "优秀";
-                break;
             case 10:
-                msg = "满分！太棒了！";
+            case 12:
+                msg = "大月";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                msg = "小月";
+                break;
+            case 2:
+                msg = "28天";
                 break;
         }
-    }else{
-        msg = "成绩输入有误！请重新输入！"
-    }
+       
     alert(msg);
 }
