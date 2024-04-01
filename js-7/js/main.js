15d7a47a01c6d2e88d0b53d888b6cc3f24aefe6d
Gerrrrrria <1440054622@qq.com>
2024-04-01 11:50:04 +0800

    修改了一些东西

--- js-7/js/main.js
+++ js-7/js/main.js
@@ -1,10 +1,60 @@
 function f1(){
-    var i = 1,sum = 0;
-    while(i <= 100){
-        if(i % 3 == 0 && i % 5 == 0){
-        console.log("1~100能同时整除3和5的数为：" + i);
-        }
-        i++;
+    show()
+}
+
+//无参无返回值
+function show(){
+    console.log("hello world!");
+}
+
+function f2(){
+    message("疯狂星期四V我50!!!")  //实参
+}
+
+//有参无返回值    形参
+function message(info){
+    console.log("您收到了一条新消息:" + info);
+}
+
+function f3(){
+    let s = add(19,5)
+    console.log(s);
+}
+
+//有参有返回值
+function add(a, b){
+    let sum  = a + b
+    return sum
+}
+
+function f4(){
+    let a = 65531, b = 4
+    let fh = '+'
+    let result;
+    result = suan_shu(a, b, fh)
+    console.log(result);
+}
+
+//算术运算：+、-、*、/、%
+
+function suan_shu(a,b,fu_hao){
+    let r = 0
+    switch(fu_hao){
+        case '+':
+            r = a + b
+            break
+        case '-':
+            r = a - b
+            break
+        case '*':
+            r = a * b
+            break
+        case '/':
+            r = a / b
+            break
+        default:
+            r = a % b
+            break
     }
- 
+    return r
 }
--- js-8/js/main.js
+++ js-8/js/main.js
@@ -1,17 +1,17 @@
 function f1(){
     let ban_ji = new Array(5) //初始长度
     
-    ban_ji[0] = '张三'
-    ban_ji[1] = '李四'
-    ban_ji[2] = '王武'
-    ban_ji[3] = '秦始皇'
-    ban_ji[4] = '汉武帝'
-    ban_ji[5] = '成吉思汗'  //数组自动扩容
+    ban_ji[0] = '肯德基'
+    ban_ji[1] = '麦当劳'
+    ban_ji[2] = '德克士'
+    ban_ji[3] = '必胜客'
+    ban_ji[4] = '星巴克'
+    ban_ji[5] = '茶百道'  //数组自动扩容
 
     console.log(ban_ji.length) //查看数组的当前长度            
 }
 function f2(){
-    let banji = ['秦始皇','汉高祖','汉武帝','唐太宗']
+    let banji = ['斯大林','罗斯福','丘吉尔','李德胜']
     
     for(let i=0;i<banji.length;i++){
         console.log(banji[i]);
@@ -25,8 +25,8 @@ function f3(){
     let avg;
     for(let i=0;i<score.length;i++){
         sum += score[i];
-        avg = sum / i;
     }
+    avg = sum / score.length;
     console.log("总分为"+sum);
     console.log("平均分为"+avg);
 }
