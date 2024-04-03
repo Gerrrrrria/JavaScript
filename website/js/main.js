438ff90ce3e58ed126b6807dc04ed0ea56c429fc
Gerrrrrria <1440054622@qq.com>
2024-04-03 11:15:26 +0800

    改改网页

--- website/js/main.js
+++ website/js/main.js
@@ -1,45 +1,53 @@
 function f1(){
-    var score = prompt("请输入成绩：");
-    var msg;
-    var re = Math.floor( score / 10 );
-    if(0 <= score && score <= 100){
-        switch(re){
-            case 0:
-                msg = "不及格";
-                break;
-            case 1:
-                msg = "不及格";
-                break;
-            case 2:
-                msg = "不及格";
-                break;
-            case 3:
-                msg = "不及格";
-                break;
-            case 4:
-                msg = "不及格";
-                break;
-            case 5:
-                msg = "不及格";
-                break;    
-            case 6:
-                msg = "及格";
-                break;
-            case 7:
-                msg = "一般";
-                break;
-            case 8:
-                msg = "良好";
-                break;
-            case 9:
-                msg = "优秀";
-                break;
-            case 10:
-                msg = "满分！太棒了！";
-                break;
-        }
-    }else{
-        msg = "成绩输入有误！请重新输入！"
-    }
-    alert(msg);
+    let s1 = 'abc'
+    let s2 = new String('abc')
+    console.log(s1.charAt(2)); //获取字符串中指定位置的字符
+}
+
+function f2(){
+    let div = document.getElementById('my_div')
+    let s = '台湾地区发生7.2级大地震，大陆汶川大地震'  // 10,18
+    let i = s.indexOf('大地震')  //从左向右第一次出现的位置
+    div.innerHTML = i
+    i = s.lastIndexOf('大地震')
+    div.innerHTML += '<p>' + i  
+    
+}
+// xx@yy.zz
+/*
+   .的索引 - @的索引 >= 2
+*/
+function f3(){
+  let div = document.getElementById('my_div')
+//获取用户输入的email，然后进行合法性检验
+  let email = document.getElementById('email').value
+  let i = email.indexOf('.') - email.indexOf('@')
+  //let cha = email.length -  email.indexOf('.')  //判断.是否为最后一个字符
+  let ok = email.endsWith('.') //是否以.结尾
+  
+//   if(i>=2 && cha != 1 ){
+//     div.innerHTML = '邮箱格式合法'
+//     div.style.color = 'green'
+//   }
+//   else{
+//     div.innerHTML = '邮箱格式非法'
+//     div.style.color = 'red'
+//   }
+    if(i>=2 && ok==false ){
+    div.innerHTML = '邮箱格式合法'
+    div.style.color = 'green'
+  }
+  else{
+    div.innerHTML = '邮箱格式非法'
+    div.style.color = 'red'
+  }
+}
+function f4(){
+//   实现中华民族伟大复兴的中国梦  china dream
+   let s = '实现中华民族伟大复兴的中国梦'
+   s = s.replace('中国梦','chinese dream')
+
+   let div = document.getElementById('my_div')
+   div.innerHTML = s
+
 }
